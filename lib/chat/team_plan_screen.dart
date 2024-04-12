3cc79e51535046c09c133ce9a97b119c613ad957
RitRible <wnsduddl0116@naver.com>
2024-04-12 15:04:28 +0900

    날짜선택 관련수정

--- lib/chat/team_plan_screen.dart
+++ lib/chat/team_plan_screen.dart
@@ -6,19 +6,18 @@ import 'package:untitled1/user_info.dart';
 import 'package:untitled1/chat/_teamplan.dart';
 
 class TeamPlanScreen extends StatelessWidget {
-  const TeamPlanScreen({super.key});
+  const TeamPlanScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-
       body: TeamPlanListScreen(),
     );
   }
 }
 
 class TeamPlanListScreen extends StatefulWidget {
-  const TeamPlanListScreen({super.key});
+  const TeamPlanListScreen({Key? key});
 
   @override
   State<TeamPlanListScreen> createState() => _TeamPlanListScreenState();
@@ -28,7 +27,7 @@ class _TeamPlanListScreenState extends State<TeamPlanListScreen> {
   DateTime? _startDate;
   DateTime? _endDate;
   List<String> _participants = [];
-  final TextEditingController _descriptionController = TextEditingController();
+  TextEditingController _descriptionController = TextEditingController();
   List<Plan> _plans = [];
 
   @override
@@ -36,6 +35,7 @@ class _TeamPlanListScreenState extends State<TeamPlanListScreen> {
     super.initState();
     _fetchPlans();
   }
+
   Future<void> _fetchPlans() async {
     final userInfo = UserInfo();
     String userEmail = userInfo.userEmail ?? '';
@@ -61,51 +61,84 @@ class _TeamPlanListScreenState extends State<TeamPlanListScreen> {
       // 예외 처리 코드 추가
     }
   }
-  Future<void> _selectDate(BuildContext context, bool isStartDate) async {
+
+  Future<DateTime?> _selectDate(BuildContext context, bool isStartDate) async {
     final DateTime? pickedDate = await showDatePicker(
       context: context,
       initialDate: DateTime.now(),
       firstDate: DateTime.now(),
       lastDate: DateTime(DateTime.now().year + 1),
     );
+
     if (pickedDate != null) {
-      setState(() {
-        if (isStartDate) {
+      // 시작일 선택 시
+      if (isStartDate) {
+        setState(() {
           _startDate = pickedDate;
-        } else {
-          _endDate = pickedDate;
+          _endDate = null; // 시작일을 선택할 때마다 종료일 초기화
+        });
+      }
+      // 종료일 선택 시
+      else {
+        // 시작일이 설정되어 있고 선택한 날짜가 시작일 이전인 경우
+        if (_startDate != null && pickedDate.isBefore(_startDate!)) {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('날짜 선택 오류'),
+                content: Text('종료일은 시작일 이후의 날짜로 선택해주세요.'),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('확인'),
+                  ),
+                ],
+              );
+            },
+          );
+          return null; // 종료일이 시작일 이전이면 선택한 날짜를 취소하고 종료
         }
-      });
+        setState(() {
+          _endDate = pickedDate;
+        });
+      }
     }
+
+    return pickedDate;
   }
 
-  void _addParticipant() {
+
+
+  void _addParticipant(StateSetter setState) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        final TextEditingController controller = TextEditingController();
+        final TextEditingController _controller = TextEditingController();
         return AlertDialog(
-          title: const Text('참여자 추가'),
+          title: Text('참여자 추가'),
           content: TextField(
-            controller: controller,
-            decoration: const InputDecoration(labelText: '이름'),
+            controller: _controller,
+            decoration: InputDecoration(labelText: '이름'),
           ),
           actions: <Widget>[
             TextButton(
               onPressed: () {
                 setState(() {
-                  _participants.add(controller.text);
-                  controller.clear(); // 입력 필드 비우기
+                  _participants.add(_controller.text);
+                  _controller.clear(); // 입력 필드 비우기
                 });
                 Navigator.of(context).pop();
               },
-              child: const Text('추가'),
+              child: Text('추가'),
             ),
             TextButton(
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: const Text('취소'),
+              child: Text('취소'),
             ),
           ],
         );
@@ -117,53 +150,60 @@ class _TeamPlanListScreenState extends State<TeamPlanListScreen> {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('새로운 계획 추가'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              _buildDateSelector('시작일', _startDate, true),
-              _buildDateSelector('종료일', _endDate, false),
-              const SizedBox(height: 16),
-              TextField(
-                controller: _descriptionController,
-                decoration: const InputDecoration(labelText: '계획 내용'),
+        return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return AlertDialog(
+              title: Text('새로운 계획 추가'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  _buildDateSelector('시작일', _startDate, true, setState),
+                  _buildDateSelector('종료일', _endDate, false, setState),
+                  SizedBox(height: 16),
+                  TextField(
+                    controller: _descriptionController,
+                    decoration: InputDecoration(labelText: '계획 내용'),
+                  ),
+                  SizedBox(height: 16),
+                  Text('참여자'),
+                  Wrap(
+                    children: _participants.map((participant) {
+                      return Padding(
+                        padding: EdgeInsets.all(4),
+                        child: Chip(
+                          label: Text(participant),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                  SizedBox(height: 8),
+                  ElevatedButton(
+                    onPressed: () => _addParticipant(setState),
+                    child: Text('참여자 추가'),
+                  ),
+                ],
               ),
-              const SizedBox(height: 16),
-              const Text('참여자'),
-              Wrap(
-                children: _participants.map((participant) {
-                  return Padding(
-                    padding: const EdgeInsets.all(4),
-                    child: Chip(
-                      label: Text(participant),
-                    ),
-                  );
-                }).toList(),
-              ),
-              const SizedBox(height: 8),
-              ElevatedButton(
-                onPressed: _addParticipant,
-                child: const Text('참여자 추가'),
-              ),
-            ],
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: _onSave,
-              child: const Text('추가'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('취소'),
-            ),
-          ],
+              actions: <Widget>[
+                TextButton(
+                  onPressed: _onSave,
+                  child: Text('추가'),
+                ),
+
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('취소'),
+                ),
+              ],
+            );
+          },
         );
       },
     );
   }
+
+
 
   void _onSave() async {
     final userInfo = UserInfo();
@@ -196,7 +236,7 @@ class _TeamPlanListScreenState extends State<TeamPlanListScreen> {
       });
       // 데이터 추가가 성공적으로 이루어졌으므로 다이얼로그를 닫습니다.
       if (mounted) {
-      Navigator.of(context).pop();
+        Navigator.of(context).pop();
       }
     } catch (e) {
       // Firestore 데이터 추가 과정에서 예외가 발생한 경우
@@ -204,7 +244,6 @@ class _TeamPlanListScreenState extends State<TeamPlanListScreen> {
       // 예외 처리 코드 추가 (예를 들어, 사용자에게 오류 메시지를 보여줄 수 있음)
     }
   }
-
 
 
   @override
@@ -281,52 +320,55 @@ class _TeamPlanListScreenState extends State<TeamPlanListScreen> {
   }
 
   Future<void> _deleteSelectedPlan(int index) async {
-  final plan = _plans[index];
+    final plan = _plans[index];
 
-  if (plan.id.isEmpty) {
-    debugPrint('Plan ID가 비어있어 삭제할 수 없습니다.');
-    return;
-  }
+    if (plan.id.isEmpty) {
+      debugPrint('Plan ID가 비어있어 삭제할 수 없습니다.');
+      return;
+    }
 
-  try {
-    final firestoreInstance = FirebaseFirestore.instance;
-    await firestoreInstance.collection('plans').doc(plan.id).delete();
+    try {
+      final firestoreInstance = FirebaseFirestore.instance;
+      await firestoreInstance.collection('plans').doc(plan.id).delete();
 
-    setState(() {
-      _plans.removeAt(index);
-    });
-  } catch (e) {
-    debugPrint('Firestore에서 데이터 삭제 중 오류 발생: $e');
-    // 예외 처리 코드 추가
-  }
-}
-
-  void _ddeletePlan(int index) {
-    setState(() {
-      _plans.removeAt(index);
-    });
+      setState(() {
+        _plans.removeAt(index);
+      });
+    } catch (e) {
+      debugPrint('Firestore에서 데이터 삭제 중 오류 발생: $e');
+      // 예외 처리 코드 추가
+    }
   }
 
   void _navigateToPlanDetails(Plan plan, int index) {
-  Navigator.push(
-    context,
-    MaterialPageRoute(
-      builder: (context) => PlanDetailsScreen(plan: plan, index: index),
-    ),
-  );
-}
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => PlanDetailsScreen(plan: plan, index: index),
+      ),
+    );
+  }
 
-  Widget _buildDateSelector(String label, DateTime? date, bool isStartDate) {
+  Widget _buildDateSelector(String label, DateTime? date, bool isStartDate, StateSetter setState) {
     return Row(
       children: <Widget>[
         Text(label),
-        const SizedBox(width: 16),
+        SizedBox(width: 16),
         ElevatedButton(
-          onPressed: () => _selectDate(context, isStartDate),
+          onPressed: () async {
+            final pickedDate = await _selectDate(context, isStartDate);
+            if (pickedDate != null) {
+              setState(() {
+                if (isStartDate) {
+                  _startDate = pickedDate;
+                } else {
+                  _endDate = pickedDate;
+                }
+              });
+            }
+          },
           child: Text(
-            date == null
-                ? '날짜 선택'
-                : DateFormat('yyyy-MM-dd').format(date),
+            date == null ? '날짜 선택' : DateFormat('yyyy-MM-dd').format(date),
           ),
         ),
       ],
