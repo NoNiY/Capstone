fe31d3dd896dc3e9df5a44dbb9f71842fc889343
dontotl18 <78009866+dontotl18@users.noreply.github.com>
2024-04-16 18:31:19 +0900

    Create store_image.dart

---
+++ lib/character/store_image.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class StoreImage {
+  static String backgroundImage = 'assets/background/background1.jpg';
+  static String characterImage = 'assets/character/character1.png';
+}
